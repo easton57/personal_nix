3dd10a5f3fa7a063a32a437d3b706adf5b0743c8
Easton Seidel <eseidel1357@gmail.com>
2025-05-25 19:37:20 -0700

    Install blender from unstable and with Cuda support

--- modules/graphics.nix
+++ modules/graphics.nix
@@ -4,6 +4,17 @@ let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in
 {
+  # Blender with Cuda
+  nixpkgs.overlays = [
+      (self: super: {
+          blenderWithCuda = unstable.blender.override {
+              cudaSupport = true;
+              cudaPackages = super.cudaPackages;
+          };
+      })
+  ];
+
+
   environment.systemPackages = [
     unstable.blender
     pkgs.godot_4
