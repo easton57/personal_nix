29833255d6eec698e665c487bc7de532b752c5f9
Easton Seidel <eseidel1357@gmail.com>
2025-05-25 19:36:57 -0700

    Re-enable nvidia open driver, correct warning from 25.05 upgrade

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -174,7 +174,7 @@
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
