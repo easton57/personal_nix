627390b0e3d9272281404f5046e0bd6d6d4c5459
Easton Seidel <eseidel1357@gmail.com>
2025-05-27 15:09:03 -0700

    Fingerprint reader

--- modules/security.nix
+++ modules/security.nix
@@ -4,5 +4,13 @@
   environment.systemPackages = [
     # Needed for gparted and other packages needing authentication
     pkgs.lxqt.lxqt-policykit
+
+    # For fingerprint
+    pkgs.fprintd
   ];
+
+  # fprintd
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 }
