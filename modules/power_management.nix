5b5285a91669f918a9056f27593b5d360575b13c
Easton Seidel <eseidel1357@gmail.com>
2025-05-28 15:52:58 -0700

    Use more power on battery

--- modules/power_management.nix
+++ modules/power_management.nix
@@ -6,6 +6,9 @@
   # Keep awake if plugged in and lid closed
   services.logind.lidSwitchExternalPower = "ignore";
 
+  # Thermal throttle protection
+  services.thermald.enable = true;
+
   # TLP enablement for laptop management
   services.tlp = {
       enable = true;
@@ -19,7 +22,7 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
+        CPU_MAX_PERF_ON_BAT = 100;
 
        # Optional helps save long term battery health
        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
