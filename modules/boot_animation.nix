50c13d6c1c9ef3bd79cf566af758e2ea2139f049
Easton Seidel <eseidel1357@gmail.com>
2025-05-28 11:25:20 -0700

    Show grub

--- modules/boot_animation.nix
+++ modules/boot_animation.nix
@@ -7,7 +7,6 @@
       enable = true;
       theme = "pixels";
       themePackages = with pkgs; [
-        # By default we would install all themes
         (adi1090x-plymouth-themes.override {
           selected_themes = [ "pixels" ];
         })
@@ -25,10 +24,8 @@
       "rd.systemd.show_status=auto"
     ];
 
-    # Hide the OS choice for bootloaders.
-    # It's still possible to open the bootloader list by pressing any key
-    # It will just not appear on screen unless a key is pressed
-    loader.timeout = 0;
+    # Show grub for 5 seconds (got a little stuck once and am now scared)
+    loader.timeout = 5;
   };
 }
 
