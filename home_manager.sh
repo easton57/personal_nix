95cd804edac880560ead728274044a45ea721b68
Easton Seidel <eseidel1357@gmail.com>
2025-06-03 11:05:27 -0700

    Home manager installer script

---
+++ home_manager.sh
@@ -0,0 +1,10 @@
+# Install nixos
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+
+# Install home manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+# Generate initial config
+nix-shell '<home-manager>' -A install
+
